61d11b7100f2a68dcb63967db5d2fe01ba1e5247
JoasE <32096708+JoasE@users.noreply.github.com>
2020-05-01 15:21:32 +0200

    Initial commit

---
+++ NSubstitute.Extensions.EntityFramework/NSubstituteEntityFrameworkExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace NSubstitute.Extensions.EntityFramework
+{
+    public static class NSubstituteEntityFrameworkExtensions
+    {
+        public static DbSet<TEntity> WithData<TEntity>(this DbSet<TEntity> mock, IEnumerable<TEntity> data)
+            where TEntity : class
+        {
+            var queryable = data.AsQueryable();
+
+            ((IQueryable<TEntity>)mock).Expression.Returns(queryable.Expression);
+            ((IQueryable<TEntity>)mock).ElementType.Returns(queryable.ElementType);
+            ((IQueryable<TEntity>)mock).GetEnumerator().Returns(queryable.GetEnumerator());
+
+            ((IDbAsyncEnumerable<TEntity>)mock).GetAsyncEnumerator()
+                .Returns(new TestDbAsyncEnumerator<TEntity>(queryable.GetEnumerator()));
+            ((IQueryable<TEntity>)mock).Provider.Returns(new TestDbAsyncQueryProvider<TEntity>(queryable.Provider));
+
+            return mock;
+        }
+
+        private static readonly MethodInfo returnsMethod = typeof(SubstituteExtensions).GetMethods().First(x => x.Name == nameof(SubstituteExtensions.Returns) && x.GetParameters().Length == 3);
+
+        public static TContext WithSet<TContext, TEntity>(this TContext context,
+            Expression<Func<TContext, DbSet<TEntity>>> propertySelector, IEnumerable<TEntity> data)
+            where TContext : DbContext
+            where TEntity : class
+        {
+            var mockSet = Substitute.For<DbSet<TEntity>, IQueryable<TEntity>, IDbAsyncEnumerable<TEntity>>().WithData(data);
+
+            // TContext x
+            var parameter = propertySelector.Parameters.First();
+
+            // x.DbSet
+            var dbSetPropertyExpression = (MemberExpression)propertySelector.Body;
+
+            // x.DbSet.Returns(mockSet)
+            var returnsCall = Expression.Call(returnsMethod.MakeGenericMethod(typeof(DbSet<TEntity>)), dbSetPropertyExpression, Expression.Constant(mockSet), Expression.Constant(null, typeof(DbSet<TEntity>[])));
+
+            // Invoke the newly build lambda, and discard it
+            Expression.Lambda<Action<TContext>>(returnsCall, parameter).Compile()(context);
+
+            return context;
+        }
+    }
+}
---
+++ NSubstitute.Extensions.EntityFramework/TestDbAsyncEnumerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSubstitute.Extensions.EntityFramework
+{
+    internal class TestDbAsyncEnumerator<T> : IDbAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> enumerator;
+
+        public TestDbAsyncEnumerator(IEnumerator<T> enumerator)
+        {
+            this.enumerator = enumerator;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public Task<bool> MoveNextAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(enumerator.MoveNext());
+        }
+
+        public T Current => enumerator.Current;
+
+        object IDbAsyncEnumerator.Current => Current;
+    }
+}
